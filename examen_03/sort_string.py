74cccb216c55f00683f4adf4a8b41f762d4ef222
Amine Elhachimi <amine2005.elhachimi@gmail.com>
2026-03-09 12:47:01 +0000

    examen

--- examen_03/sort_string.py
+++ examen_03/sort_string.py
@@ -1,2 +1,15 @@
 def sort_string(s: str) -> str:
-    return filter(s, lambda s: s.isdigit(), s.isupper(), s.islower(), s)
+    return ''.join(sorted(s, key=lambda char: (char.isdigit(), char.isupper(), char.islower()), reverse=True))
+
+
+def count_vowels(word):
+    return sum(1 for c in word.lower() if c in "aeiou")
+
+
+def sort_string_by():
+    words = ["Cat", "bat", "apple", "to", "Tea"]
+    return sorted(words, key=lambda x: (len(x), x.lower(), count_vowels(x)))
+
+
+print(sort_string_by())
+# print(sort_string("aB3cA1!"))
